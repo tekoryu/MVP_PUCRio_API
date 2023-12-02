9f453d476147a22ad959deac64642830a984bc0a
Anderson Monteiro <chedou@gmail.com>
2023-12-02 15:03:00 -0300

    exporta para arquivo

--- main.py
+++ main.py
@@ -9,29 +9,37 @@ Additional Information:
 - The goal public is Legislative Assessment bodies in Brazil. This version
 is refined only to Senate propositions.
 """
+import datetime
+
 from pdfminer.high_level import extract_text, extract_pages
 from pdfminer.layout import LTTextContainer, LAParams
 
+from convert_pdf import convert_pdf
 
-def box_analyzer(element: LTTextContainer):
-    texticulo = element.get_text()
-    artigos
 
 def convert_pdf_to_text(pdf_file: str, footer: float, header: float):
     #   Lê um arquivo PDF e retorna texto cru
 
-    output_string = []
+    output_list = []
+    output_string = ""
+
     laparams = LAParams(line_overlap=0.1)
     for page_layout in extract_pages(pdf_file, laparams=laparams):
         for element in page_layout:
             if isinstance(element, LTTextContainer):
                 if footer < element.y0 < header:
-                    output_string.append(element)
-                    # output_string = output_string + element.get_text()
+                    output_list.append(element)
+                    output_string = output_string + element.get_text()
 
     return output_string
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    texto = convert_pdf_to_text("training_files/Projeto de Lei.pdf", 70, 800)
+    timestamp = datetime.datetime.timestamp(datetime.datetime.now())
 
-    convert_pdf_to_text("training_files/Projeto de Lei.pdf", 70, 800)
+    # output to file
+    text_file = open(f"outputs/data{timestamp}.txt", "w+")
+    text_file.write(texto)
+    text_file.close()
